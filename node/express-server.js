7439c276e88e98cc987ff5efdd206be44af15a13
Hunter, Kirsten <khunter@akamai.com>
2016-01-26 03:58:00 +0530

    Hapi-ness

--- node/express-server.js
+++ node/express-server.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var app = express();
 var router = express.Router();    
+var path = require('path');
+var bodyParser = require('body-parser');
+
+app.use(express.static(path.join(__dirname, 'static')));
+app.use(bodyParser.json());
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
 var db = mongoose.connection;
 
-
 var quoteSchema = mongoose.Schema({
-        name: String,
+        content: String,
         author: String,
         id: Number
 });
@@ -19,25 +23,43 @@ Quote.count().exec(function(err, count){
    quotecount = count;
 });
 
-
 // REST API
-router.route('/quotes')
+router.route('/quotes/random')
 .get(function(req, res, next) {
-  if (req.query.random) {
     var random = Math.floor(Math.random() * quotecount);
     Quote.findOne({"id":random},
     function (err, result) {
-	res.send(result);
-    });
-  } else {
-    Quote.find(
+      if (err) {
+        console.log(err);
+        res.redirect('/quotes/random');
+      }
+     res.send(result);
+    })
+});
+
+router.route('/quotes')
+.get(function(req, res, next) {
+    Quote.find({}, {}, {limit:10},
       function (err, result) {
          res.send(result);
-      }
-   )};
+   });
 })
 .post(function(req, res, next) {
-  res.send('Post');
+  if(!req.body.hasOwnProperty('content')) {
+    res.statusCode = 400;
+    return res.send('Error 400: Post syntax incorrect.');
+  }
+  quotecount = quotecount+1; 
+  var newQuote;
+  if (req.body.hasOwnProperty('author')) {
+    newQuote = new Quote({'content': req.body.content, 'author': req.body.author, 'id': quotecount});
+  } else {
+    newQuote = new Quote({'content': req.body.content, 'id':quotecount});
+  }
+  newQuote.save(function (err, newQuote) {
+    if (err) return console.error(err);
+    res.json(quotecount);
+  });;
 });
 
 router.route('/quotes/:id')
@@ -48,19 +70,42 @@ router.route('/quotes/:id')
     });
 })
 .put(function(req, res, next) {
-  res.send('Put id: ' + req.params.id);
+  if(!req.body.hasOwnProperty('content') && (!req.body.hasOwnProperty('author'))) {
+    res.statusCode = 400;
+    return res.send('Error 400: Post syntax incorrect.');
+  }
+  var query = {'id':req.params.id};
+  var newQuote = new Quote();
+  if (req.body.hasOwnProperty('author')) {
+        newQuote.author = req.body.author;
+  };
+  if (req.body.hasOwnProperty('content')) {
+        newQuote.content = req.body.content;
+  };
+  var upsertData = newQuote.toObject();
+  delete upsertData._id;
+  Quote.findOneAndUpdate(query, upsertData, {upsert:true}, function(err, doc){
+    if (err) return res.send(500, { error: err });
+    return res.send("succesfully saved");
+  });
 })
 .delete(function(req, res, next) {
-  res.send('Delete id: ' + req.params.id);
+   Quote.findOneAndRemove({"id":req.params.id},
+    function (err, result) {
+        if (!err) {
+           res.json(true);
+        }
+    });
 });
 
 app.use('/api', router);
 
 // index
 app.get('/', function(req, res) {
-  res.send('Hello world');
+  res.send('Hello world from express');
 });
 
 var server = app.listen(3000, function() {
   console.log('Express is listening to http://localhost:3000');
 });
+
---
+++ node/hapi-server.js
@@ -0,0 +1,125 @@
+// *******************************************
+// DATABASE STUFF ****************************
+// *******************************************
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+
+var quoteSchema = mongoose.Schema({
+        content: String,
+        author: String,
+        id: Number
+});
+
+var quotecount;
+var Quote = mongoose.model('Quote', quoteSchema)
+Quote.count().exec(function(err, count){
+   quotecount = count;
+});
+// *******************************************
+
+var Hapi = require('hapi');
+var server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.route([
+  {
+    method: 'GET',
+    path: '/api/quotes/random',
+    handler: function(request, reply) {
+      var random = Math.floor(Math.random() * quotecount);
+      Quote.findOne({"id":random},
+      function (err, result) {
+        reply(result);
+      })
+    }
+  },
+{
+    method: 'GET',
+    path: '/api/quotes',
+    handler: function(request, reply) {
+      Quote.find({}, {}, {limit:10},
+      function (err, result) {
+         reply(result);
+      });
+    }
+  },
+ {
+    method: 'POST',
+    path: '/api/quotes',
+    handler: function(request, reply) {
+        if(!request.payload.content) {
+          return reply('Error 400: Post syntax incorrect.').code(400);
+        }
+        quotecount = quotecount+1; 
+        var newQuote;
+        if (request.payload.author) {
+          newQuote = new Quote({'content': request.payload.content, 'author': request.payload.author, 'id': quotecount});
+        } else {
+         newQuote = new Quote({'content': request.payload.content, 'id':quotecount});
+        }
+        newQuote.save(function (err, newQuote) {
+          if (err) return console.error(err);
+          reply(quotecount);
+      });
+    }
+  },
+  {
+    method: 'GET',
+    path: '/api/quotes/{id}',
+    handler: function(request, reply) {
+    Quote.findOne({"id":request.params.id},
+      function (err, result) {
+        reply(result);
+      });
+    }
+  },
+{
+    method: 'PUT',
+    path: '/api/quotes/{id}',
+    handler: function(request, reply) {
+        if(!request.body.hasOwnProperty('content') && (!request.body.hasOwnProperty('author'))) {
+          return reply('Error 400: Post syntax incorrect.').code(400);
+        }
+        var query = {'id':request.params.id};
+        var newQuote = new Quote();
+        if (request.body.hasOwnProperty('author')) {
+         newQuote.author = request.body.author;
+        };
+        if (request.body.hasOwnProperty('content')) {
+         newQuote.content = request.body.content;
+        };
+        var upsertData = newQuote.toObject();
+        delete upsertData._id;
+        Quote.findOneAndUpdate(query, upsertData, {upsert:true}, function(err, doc){
+          if (err)  {
+            return reply({ error: err }).code(500);
+          }
+          return reply("succesfully saved");
+        });
+    }
+  },
+  {
+    method: 'DELETE',
+    path: '/api/quotes/{id}',
+    handler: function(request, reply) {
+        Quote.findOneAndRemove({"id":request.params.id},
+          function (err, result) {
+            if (!err) {
+              reply(true);
+            }
+        });
+    }
+  },
+  {
+    method: 'GET',
+    path: '/',
+    handler: function(request, reply) {
+      reply('Hello world from hapi');
+    }
+  }
+]);
+
+server.start(function() {
+  console.log('Hapi is listening to http://localhost:3000');
+});
