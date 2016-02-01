e03e89586d0f878fc4f474d12bcdd252318ea1c9
Hunter, Kirsten <khunter@akamai.com>
2016-02-01 09:11:47 -0800

    Updating the perl client to have all actions

--- perl/dancer-server.pl
+++ perl/dancer-server.pl
@@ -16,7 +16,7 @@ get '/' => sub{
 
 get '/api/quotes' => sub {
     my $response = $quotes->find()->sort({'index' => -1})->limit(10);
-    my @results ;
+    my @results = ();
     while(my $quote = $response->next) {
                 push (@results,
                         {"content" => $quote->{'content'},
@@ -25,21 +25,31 @@ get '/api/quotes' => sub {
                          }
                 );
         } 
+        if (! scalar (@results)) {
+            status 404;
+            return;
+        }
         return \@results;
 };
 
 post '/api/quotes' => sub {
-    my $response = $quotes->find()->sort({'index' => -1})->limit(10);
-        my @results ;
-        while(my $quote = $response->next) {
-                push (@results,
-                        {"content" => $quote->{'content'},
-                         "index"   => $quote->{'index'},
-                         "author"  => $quote->{'author'}
-                         }
-                );
-        } 
-        return \@results;
+    my $max_id = $quotes->count() + 1;
+    # get the author and content from the parameters
+    if (!params->{content}) {
+        status 400;
+        return {message => "Content is required for new quotes."};
+    }
+
+    my %response = (
+        'author' => params->{author},
+        'content' => params->{content},
+        'id' => $max_id
+    );
+
+    my $response = $quotes->insert_one(\%response);
+
+    status 201;
+    return ;
 };
 
 
@@ -57,13 +67,40 @@ get '/api/quotes/random' => sub {
 
 get '/api/quotes/:index' => sub {
     
-    my $response = $quotes->find({"index" => int(params->{'index'})});  
+    my $response = $quotes->find({"index" => int(params->{'index'})}); 
     while(my $quote = $response->next) {
-    return {"content" => $quote->{'content'},
+        return {"content" => $quote->{'content'},
             "index"   => $quote->{'index'},
             "author"  => $quote->{'author'}
         };
     };
+    status 404;
+    return;
+};
+
+put '/api/quotes/:index' => sub {
+    if (!params->{content} && !params->{author}) {
+        status 400;
+        return {message => "Content or author is required for updated quotes."};
+    }
+    my $original = $quotes->find({index => int(params->{'index'})}); 
+    my $author = $original->{author};
+    my $content = $original->{content};
+    if (params->{author}) { $author = params->{author}}
+    if (params->{content}) { $content = params->{content}}
+    
+    my $response = $quotes->update_one(encode_json({'index' => params->{index}}), 
+                        {'$set' => {'author'=>$author, 'content'=>$content}});
+
+    status 202;
+    return;
+};
+
+del '/api/quotes/:index' => sub {
+    my $response = $quotes->delete_one({index => int(params->{'index'})});
+
+    status 204;
+    return;
 };
 
 
